18ac89b7c6e1260b11babeab550b9ae93a792a34
emin <azamovemin2@gmail.com>
2023-06-22 17:08:38 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+let x
+let chance = 3
+let count = 0
+
+do {
+    alert(`${chance - count}`)
+    x = prompt('Password')
+    if (x === '7777') {
+        break;
+
+    } 3
+    count++
+
+} while (count !== 3)
+
+// if (x === '7777') {
+//     break;
+// }
